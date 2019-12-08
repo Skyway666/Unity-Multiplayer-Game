1eedca58bcbbf594f481ca863090e72d3a966245
Skyway666 <lucasgarciamateu@gmail.com>
2019-12-08 18:12:10 +0100

    Improved drone rotation

--- Multiplayer Game Unity/Assets/Scripts/DroneScripts/DroneController.cs
+++ Multiplayer Game Unity/Assets/Scripts/DroneScripts/DroneController.cs
@@ -12,7 +12,10 @@ public class DroneController : NetworkBehaviour
     const float MAX_TILT_ANGLE = 20.0f;
 
     const float Y_ROTATION_SPEED = 180.0f;
-    const float X_ROTATION_SPEED = 30.0f;
+    const float X_ROTATION_SPEED = 60.0f;
+
+
+    Vector3 customEulerAngles = new Vector3(0,0,0);
 
     // Name sync /////////////////////////////////////
     [SyncVar(hook = "SyncNameChanged")]
@@ -64,7 +67,9 @@ public class DroneController : NetworkBehaviour
 
         if (!isLocalPlayer) return;
 
-        Vector3 translation = new Vector3();
+
+        Vector3 Forward2d = (new Vector3(transform.forward.x, 0, transform.forward.z)).normalized;
+        Vector3 Right2d = (new Vector3(transform.right.x, 0, transform.right.z)).normalized;
         float angle = 0.0f;
 
 
@@ -83,60 +88,56 @@ public class DroneController : NetworkBehaviour
         // Forward
         if (LeftAxis.y > 0.0 )
         {
-            translation += new Vector3(0.0f, 0.0f, LeftAxis.y * MOVEMENT_SPEED * Time.deltaTime);
-            transform.Translate(translation);
+            transform.position += Forward2d * LeftAxis.y * MOVEMENT_SPEED * Time.deltaTime;
 
             if (transform.rotation.eulerAngles.x < MAX_TILT_ANGLE || transform.rotation.eulerAngles.x > 180)
-                transform.Rotate(new Vector3(X_ROTATION_SPEED * Time.deltaTime, 0, 0));
+                transform.Rotate(new Vector3(1, 0, 0), X_ROTATION_SPEED * Time.deltaTime);
 
-            Debug.Log(transform.rotation.eulerAngles.x);
         }
         // Backwards
         else if (LeftAxis.y < 0.0)
         {
-            translation += new Vector3(0.0f, 0.0f, LeftAxis.y * MOVEMENT_SPEED * Time.deltaTime);
-            transform.Translate(translation);
-        
-
+            transform.position += Forward2d * LeftAxis.y * MOVEMENT_SPEED * Time.deltaTime;
 
             if (transform.rotation.eulerAngles.x > 360 - MAX_TILT_ANGLE || transform.rotation.eulerAngles.x < 180)
-                transform.Rotate(new Vector3(-X_ROTATION_SPEED * Time.deltaTime, 0, 0));
-
-            Debug.Log(transform.rotation.eulerAngles.x);
+                transform.Rotate(new Vector3(1, 0, 0), -X_ROTATION_SPEED * Time.deltaTime);
         }
 
         // Right
         if (LeftAxis.x > 0.0f)
         {
-            translation += new Vector3(LeftAxis.x * MOVEMENT_SPEED * Time.deltaTime, 0.0f, 0.0f );
-            transform.Translate(translation);
+            transform.position += Right2d * LeftAxis.x * MOVEMENT_SPEED * Time.deltaTime;
+
+            if (transform.rotation.eulerAngles.z > 360 - MAX_TILT_ANGLE || transform.rotation.eulerAngles.z < 180)
+                transform.Rotate(new Vector3(0, 0, 1), -X_ROTATION_SPEED * Time.deltaTime);
         }
         // Left
         else if (LeftAxis.x < 0.0f)
         {
-            translation += new Vector3(LeftAxis.x * MOVEMENT_SPEED * Time.deltaTime, 0.0f, 0.0f);
-            transform.Translate(translation);
+            transform.position += Right2d * LeftAxis.x * MOVEMENT_SPEED * Time.deltaTime;
+
+            if (transform.rotation.eulerAngles.z <  MAX_TILT_ANGLE || transform.rotation.eulerAngles.z > 180)
+                transform.Rotate(new Vector3(0, 0, 1), X_ROTATION_SPEED * Time.deltaTime);
         }
 
         // Righ axis
         // Up
         if(RightAxis.y > 0.0f)
         {
-            translation += new Vector3(0.0f, RightAxis.y * MOVEMENT_SPEED * Time.deltaTime, 0.0f);
-            transform.Translate(translation);
+            transform.Translate(new Vector3(0.0f, RightAxis.y * MOVEMENT_SPEED * Time.deltaTime, 0.0f));
         }
         // Down
         else if(RightAxis.y < 0.0f)
         {
-            translation += new Vector3(0.0f, RightAxis.y * MOVEMENT_SPEED * Time.deltaTime, 0.0f);
-            transform.Translate(translation);
+            transform.Translate(new Vector3(0.0f, RightAxis.y * MOVEMENT_SPEED * Time.deltaTime, 0.0f));
         }
-
+        // Rotate right
         if (RightAxis.x > 0.0f)
         {
             angle = RightAxis.x * Time.deltaTime * Y_ROTATION_SPEED;
             transform.Rotate(new Vector3(0.0f, 1.0f, 0.0f), angle);
         }
+        // Rotate left
         else if (RightAxis.x < 0.0f)
         {
             angle = RightAxis.x * Time.deltaTime * Y_ROTATION_SPEED;
@@ -145,11 +146,19 @@ public class DroneController : NetworkBehaviour
 
 
 
+        // Stabilization
 
+
+
+
+
+        // Force euler angles
+
+        //transform.rotation = Quaternion.Euler(customEulerAngles.x, customEulerAngles.y, customEulerAngles.z);
 
         if (mainCamera)
         {
-            Vector3 offset = (-transform.forward * 8);
+            Vector3 offset = -Forward2d * 8;
             offset.y += 5;
             mainCamera.transform.SetPositionAndRotation(transform.position + offset, Quaternion.identity);
             mainCamera.transform.LookAt(transform.position + new Vector3(0.0f, 0.0f, 0.0f), Vector3.up);
