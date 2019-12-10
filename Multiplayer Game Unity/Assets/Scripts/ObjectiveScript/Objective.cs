65f8e43d0e540d4bee28b90d2b3a32c2374322e7
Skyway666 <lucasgarciamateu@gmail.com>
2019-12-10 12:34:13 +0100

    Created objective prefab

---
+++ Multiplayer Game Unity/Assets/Scripts/ObjectiveScript/Objective.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Objective : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(1, 0, 0), 90 * Time.deltaTime);
+        transform.position += new Vector3(0, 3 * Time.deltaTime, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collision!");
+        Destroy(gameObject);
+        Destroy(other.gameObject);
+    }
+}
